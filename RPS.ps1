f7819c61dcb7b8219b79df0d799c790ab0aafffd
cielgoldenflame <chris.lichner@outlook.com>
2020-03-13 20:58:22 -0400

    making progress

--- RPS.ps1
+++ RPS.ps1
@@ -9,3 +9,23 @@ Plan
 #>
 
 $global:computer = 'rock','paper','scissors'
+$Global:user = $null
+
+function Computer {
+  Get-Random -InputObject $global:computer
+}
+function User {
+    Clear-Host
+    $Global:user = Read-Host "Choose your weapon`n(R)ock`n(P)aper`n(S)cissors"
+    if (($Global:user -ne 'r') -and ($Global:user -ne 'p') -and ($Global:user -ne 's')) {
+        return "You typed $Global:user"
+    }        
+    switch ($Global:user) {
+        'r' {$Global:user = 'rock'}
+        'p' {$Global:user = 'paper'}
+        's' {$Global:user = 'scissors'}
+    }
+}
+
+User
+$Global:user;Computer
