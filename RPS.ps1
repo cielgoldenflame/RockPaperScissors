389b6b762eefa9a84c9c58946d6d65dbb03be018
cielgoldenflame <chris.lichner@outlook.com>
2020-03-13 20:36:49 -0400

    creation of main file

---
+++ RPS.ps1
@@ -0,0 +1,11 @@
+<#
+Plan 
+    computer var stores options for computer
+    function for computer choose
+        get-random
+    read-host = var
+    switches?
+        if & elseif
+#>
+
+$global:computer = 'rock','paper','scissors'
